0f2e9eadd65cb121a1e218ca9bd7cf358719c9ff
RiazTurhan <turhanuddin19711@gmail.com>
2022-07-24 20:15:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let menu = document.querySelector("#menu-bars");
+let navber = document.querySelector(".navber");
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navber.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navber.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+  });
